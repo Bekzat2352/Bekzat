941f8e1b15272b653f4241d9ad3b46a73c7af8cd
Bekzat Abdrazak <abdrazakbekzat@gmail.com>
2019-11-19 14:06:58 +0600

    day1

---
+++ task33.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    double x,y;
+    cin>>x>>y;
+    if(x>=0 && y>=0){
+        cout<<sqrt(x-sqrt(y));
+    }
+return 0;
+}
